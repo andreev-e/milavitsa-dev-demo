efe90d4ac7e7c982a82a6cbe7de4dae15a45c335
Evgeniy Andreev <andreev-e@mail.ru>
2022-02-01 14:39:57 +0300

    pixel ok

--- resources/views/emails/mailing/default.blade.php
+++ resources/views/emails/mailing/default.blade.php
@@ -9,7 +9,6 @@
 
 Спасибо за внимание,<br>
 {{ config('app.name') }}
-{{ config('app.url') }}/pixel/{{ $pixel }}
 <img src="{{ config('app.url') }}/pixel/{{ $pixel }}" alt="p"/>
 <!-- Шаблон default -->
 @endcomponent
